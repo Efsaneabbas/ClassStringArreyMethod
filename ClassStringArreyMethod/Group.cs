0e9e24f1fdf311ab91c163ef3b6fe2a462cf05ec
Efsane <Efsane@DESKTOP-TMI95QM>
2024-05-13 01:25:27 +0400

    Add project files.

---
+++ ClassStringArreyMethod/Group.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassStringArreyMethod
+{
+    public class Group
+    {
+        public int No { get; set; }
+        public int StudentLimit  { get; set; }
+        public int StudentCount { get; set; }
+
+        
+        Student[] students;
+       
+        public void AddStudent(Student student)
+        {
+            if (StudentCount < StudentLimit)
+            {
+                Student[StudentCount] = student;
+                StudentCount++;
+            }
+            else
+            {
+                Console.WriteLine("Telebe Limiti Asildi");
+            }
+        }
+    }
+}
---
+++ ClassStringArreyMethod/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace ClassStringArreyMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Group group = new Group();
+
+            int choice;
+            do
+            {
+                Console.WriteLine("Menu:");
+                Console.WriteLine("1. Tələbə əlavə et");
+                Console.WriteLine("2. Bütün tələbələrə bax");
+                Console.WriteLine("3. Tələbələr üzrə axtarış et");
+                Console.WriteLine("0. Proqramı bitir");
+                Console.WriteLine("Seçiminizi edin:");
+                choice = Convert.ToInt32(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Tələbə məlumatlarını daxil edin:");
+                        Console.Write("FullName: ");
+                        string fullName = Console.ReadLine();
+                        Console.Write("GroupNo: ");
+                        string groupNo = Console.ReadLine();
+                        Console.Write("AvgPoint: ");
+                        double avgPoint = Convert.ToDouble(Console.ReadLine());
+                        Student student = new Student(fullName, groupNo, avgPoint);
+                        group.AddStudent(student);
+                        break;
+                    case 0:
+                        Console.WriteLine("Proqramı bağladınız.");
+                        break;
+                    default:
+                        Console.WriteLine("Yanlış seçim. Yenidən seçin.");
+                        break;
+                }
+            } while (choice != 0);
+        }
+    }
+}
